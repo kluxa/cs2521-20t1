a0cc37ee75e51fe6bb6f0e79d2c5ac6690343225
Kevin Luxa <k.luxa@unsw.edu.au>
2020-04-03 17:22:13 +1100

    week 7 friday

--- wk07/fri15/q1/reachable.c
+++ wk07/fri15/q1/reachable.c
@@ -5,9 +5,24 @@
 #include "Graph.h"
 #include "Set.h"
 
+void doReachable(Graph g, Vertex v, Set s);
+
 Set reachable(Graph g, Vertex v) {
 	Set s = SetNew();
-	
-
+	doReachable(g, v, s);
 	return s;
+}
+
+// DFS
+// visit vertex v
+// perform DFS from vertex v
+void doReachable(Graph g, Vertex v, Set s) {
+	// how to 'visit' vertex v?
+	SetAdd(s, v);
+	// finding neighbours
+	for (Vertex w = 0; w < GraphNumVertices(g); w++) {
+		if (GraphIsAdjacent(g, v, w) && !SetContains(s, w)) {
+			doReachable(g, w, s);
+		}
+	}
 }
